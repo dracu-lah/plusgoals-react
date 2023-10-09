8717531299c09299eee6199e4bb8a2dec0740e34
dracu-lah <nevilnicks4321@gmail.com>
2023-10-09 16:46:27 +0530

    fix: some styles, added extra inputs page: signin

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,53 +1,59 @@
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import { CheckCircleIcon, KeyIcon } from "@heroicons/react/24/solid";
-import { useRef } from "react";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+import PlusGoalsLogo from "@assets/images/plus-goal-logo.png"
 
 export default function SignIn() {
-    const emailRef = useRef(null)
-    const passwordRef = useRef(null)
-    console.log('emailRef', emailRef)
-    console.log('passwordRef', passwordRef)
+
     return (
-        <div className="min-h-screen bg-slate-400/10  flex justify-center items-center">
+        <div className="min-h-screen bg-slate-400/10  flex justify-center items-center overflow-hidden">
             <div className="bg-white p-14 shadow-md rounded-lg">
                 <div className=" text-center mb-10">
-                    <h1 className="bg-red-400/10 p-4 mb-2  ">Logo here</h1>
-                    <p>Login to access your PlusGoals Account</p>
+                    <img className="m-auto mb-4 h-12 w-56 " src={PlusGoalsLogo} alt="" />
+                    <p className="font-semibold text-primary text-xl">Login to access your PlusGoals Account</p>
                 </div>
                 <div className="space-y-4">
-                    <div className="bg-green-100/40 p-2 border border-green-400/40 rounded-md flex justify-between gap-x-2 items-start ">
-                        <CheckCircleIcon className="h-8 w-8 text-green-500" />
-                        <p className="w-80  text-justify tracking-tighter ">An email with password reset instructions has been sent to your email address, if it exists on our system.</p>
+                    <div className="bg-green-100/40 p-2 border border-green-400/40 rounded-md flex flex-col md:flex-row justify-center items-center gap-x-2 ">
+                        <CheckCircleIcon className="h-14 w-14 text-green-500" />
+                        <p className="  text- tracking-tighter ">An email with password reset instructions has been sent to your email address, if it exists on our system.</p>
                     </div>
                 </div>
                 <form action="" className="my-4">
                     <div className="space-y-4">
+                        <div className="flex flex-col   md:flex-row gap-x-4">
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">First Name:</label>
+                                <input type="text" placeholder="john" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
+                            </div>
 
-                        <div>
-                            <label htmlFor="" className="">Email Address:</label>
-                            <div className="flex items-center justify-start border rounded-md  p-2">
-                                <EnvelopeIcon className="h-4 w-4" />
-                                <input type="text" ref={emailRef} placeholder="john@doe.com" className="outline-none ml-2" />
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">Last Name:</label>
+                                <input type="text" placeholder="doe" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
                             </div>
                         </div>
-                        <div >
-                            <label htmlFor="">Password:</label>
-                            <div className="flex rounded-md  items-center justify-start border  p-2">
-                                <KeyIcon className="h-4 w-4" />
-                                <input ref={passwordRef} placeholder="Enter your password" type="password" className="outline-none ml-2" />
+                        <div className="flex flex-col   md:flex-row gap-x-4">
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">Email ID:</label>
+                                <input type="email" placeholder="john" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
+                            </div>
+
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">Phone Number:</label>
+                                <input type="text" placeholder="doe" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
                             </div>
                         </div>
-                        <div >
-                            <label htmlFor="">Re - Password:</label>
-                            <div className="flex rounded-md  items-center justify-start border  p-2">
-                                <KeyIcon className="h-4 w-4" />
-                                <input ref={passwordRef} placeholder="Enter your password again" type="password" className="outline-none ml-2" />
+                        <div className="flex flex-col md:flex-row gap-x-4">
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">Password:</label>
+                                <input type="password" placeholder="******" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <label htmlFor="" className="">Re-Password:</label>
+                                <input type="password" placeholder="******" className="outline-none border focus:border-blue-400 hover:border-blue-400 duration-300  rounded-md  p-2 " />
                             </div>
                         </div>
                         <button className="bg-blue-600/90 text-white w-full p-2 rounded-lg hover:bg-blue-600/70 duration-300">Sign In</button>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center mt-2">
                         <h6 className="hover:underline cursor-pointer">Forgot password?</h6>
                         <p>Have an account? <Link to={"/login"} className="underline cursor-pointer">Login!</Link></p>
                     </div>
